f432ae07f47e921b39d050f4c731eafeeed01c86
Fabio Coradini <fabio@Fabio-MacBook-Pro.local>
2022-06-29 15:02:39 -0300

    Add files

---
+++ build/precache-manifest.fe7f7098dca05c3bf7b26c83c5075846.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c4627361305e2fee5aa27cc6620a046",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1031e496157181fcb7a",
+    "url": "/static/css/2.b62f5c36.chunk.css"
+  },
+  {
+    "revision": "c1031e496157181fcb7a",
+    "url": "/static/js/2.dc88a9e9.chunk.js"
+  },
+  {
+    "revision": "f355aa44e76292a01be8",
+    "url": "/static/js/main.6c54be81.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
