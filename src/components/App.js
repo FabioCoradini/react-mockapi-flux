6c807e5204710373abc338c355c36d2e60032529
Fabio Coradini <fabiosrc@gmail.com>
2022-07-22 12:03:21 -0300

    add redux-persist setup finish

--- src/components/App.js
+++ src/components/App.js
@@ -9,26 +9,27 @@ import ManageCoursePage from "./ManageCoursePage";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { Provider } from "react-redux";
-import configureStore from "../store/configureStore";
-
-const store = configureStore();
+import { store, persistor } from "../store/configureStore";
+import { PersistGate } from "redux-persist/integration/react";
 
 function App() {
   return (
     <Provider store={store}>
-      <div className="container-fluid">
-        <ToastContainer autoClose={3000} hideProgressBar />
-        <Header />
-        <Switch>
-          <Route path="/" exact component={HomePage} />
-          <Route path="/courses" component={CoursesPage} />
-          <Route path="/about" component={AboutPage} />
-          <Route path="/course/:slug" component={ManageCoursePage} />
-          <Route path="/course" component={ManageCoursePage} />
-          <Redirect from="/about-page" to="about" />
-          <Route component={NotFoundPage} />
-        </Switch>
-      </div>
+      <PersistGate loading={null} persistor={persistor}>
+        <div className="container-fluid">
+          <ToastContainer autoClose={3000} hideProgressBar />
+          <Header />
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/courses" component={CoursesPage} />
+            <Route path="/about" component={AboutPage} />
+            <Route path="/course/:slug" component={ManageCoursePage} />
+            <Route path="/course" component={ManageCoursePage} />
+            <Redirect from="/about-page" to="about" />
+            <Route component={NotFoundPage} />
+          </Switch>
+        </div>
+      </PersistGate>
     </Provider>
   );
 }
